6e3a2a460a7bcd61733cf2db62fcd00ccdb3503a
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-05-28 20:45:02 +0300

    controllers auth

---
+++ controllers/auth.mjs
@@ -0,0 +1,71 @@
+import { User } from "../models/user.mjs";
+import { HttpError } from "../helpers/HttpError.js";
+import { ctrlWrapper } from "../helpers/ctrlWrapper.mjs";
+import { nanoid } from "nanoid";
+import jwt from "jsonwebtoken";
+import bcryptjs from "bcryptjs";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { SECRET_KEY } = process.env;
+
+export const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+
+  const hashPassword = await bcryptjs.hash(password, 10);
+  const verificationToken = nanoid();
+
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    verificationToken,
+  });
+
+  res.status(201).json({
+    user: { email: newUser.email },
+  });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const passwordCompare = await bcryptjs.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const payload = { id: user._id };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
+
+  res.json({
+    token,
+    user: { email: user.email },
+  });
+};
+
+export const getCurrent = async (req, res) => {
+  const { email } = req.user;
+  res.json({ email });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+  res.status(204).json();
+};
+
+export default {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  logout: ctrlWrapper(logout),
+  getCurrent: ctrlWrapper(getCurrent),
+};
